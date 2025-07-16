d186729abcc5d5f582b84770d9468b2a40536908
Amir Abaris <thisisamirabaris@gmail.com>
2025-07-16 17:14:51 +0330

    feat: basic syntax

--- logic/main.swift
+++ logic/main.swift
@@ -1,3 +1,57 @@
 import Foundation
 
-print("i love xcode :)")
+// this is a personal guide for me to master swift. why? cuz i LOVE it.
+// source of docs: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/thebasics/
+
+// the basics
+// constants and variables
+
+// constant:
+let thisIsAConstantThatItValCannotBeChanged = "value"
+
+// variables:
+var thisCanBeChanged = 1
+
+// for example:
+if thisCanBeChanged == 1 {
+    thisCanBeChanged = 2
+}
+
+// lets see the change
+print(thisCanBeChanged)
+
+// also we can add types to vars and constants:
+let number: Int = 2
+var string: String = "hi"
+
+// can do multiple values at one line:
+var n: Int = 2, s: String = "hi" // its just like c++ + typescript lmao
+
+// string interpolation, damn, its a hard name:
+print("the value of n is \(n)")
+
+// what if we wnated to make float to int?
+let fl = 3.14
+let intResult = Int(fl)
+
+print(intResult)
+
+// and the final boss of swift lol
+// if lets
+let someShit: String? = "Swift xoxo"
+
+// we declare a NEW let and put the constant we want to check in that new let!
+if let unwrappedString = someShit {
+    print("OK, its has some value \(unwrappedString)")
+}
+
+// guards:
+func doSomeShit(number: Int?) -> Int? {
+    // little bit pain, but should use to it, its man use is else! remeber that!
+    guard let unwrappedNumber = number else {
+        return nil
+    }
+    
+    return unwrappedNumber
+}
+
